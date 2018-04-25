c1707074a347c416ccdf8ee5398d648e94917c78
ElasticDuckCode <jmmillhiser@gmail.com>
2018-04-24 21:55:26 -0700

    done

--- dice.py
+++ dice.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
-
 import random
+import sys
 
-diceSelect = 0 # Sided Dice the User Picks
-
+# MAIN
+diceSelect = int(input("How Many Sides Do You Want On Your Dice? (1 or greater) "))
+print(diceSelect, "sided di(c)e selected")
+roll = "y"
+while (roll != "n"):
+    roll = input("Roll? (y/n): ")
+    if (roll == "y"):
+        print(random.randrange(1,diceSelect+1,1))
+    elif (roll == "n"):
+        sys.exit("Goodbye")
+    else:
+        print("Bad Input")
